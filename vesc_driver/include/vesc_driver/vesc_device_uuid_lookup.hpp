63725a9b790d56a51f38b663116010eacdfaad88
Andrea Scipione <andrea.scipione@gmail.com>
2023-10-20 20:41:29 +0000

    fix device uuid lookup

--- vesc_driver/include/vesc_driver/vesc_device_uuid_lookup.hpp
+++ vesc_driver/include/vesc_driver/vesc_device_uuid_lookup.hpp
@@ -36,6 +36,7 @@ public:
   const char * deviceUUID() const;
   const char * version() const;
   const char * hwname() const;
+  const char *   get_error() const;
   void close();
   bool isReady();
 
